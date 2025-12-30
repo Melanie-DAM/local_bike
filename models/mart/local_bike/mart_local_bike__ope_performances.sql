a953dab1ea8dbf362d235ae7861a7a65d7432c71
Melanie-DAM <melanie.dam@hotmail.fr>
2025-12-30 17:51:31 +0100

    Mart models (#7)
    * update
    
    * Mart def
    
    ---------
    
    Co-authored-by: melanie.dam@icloud.com <melanie.dam@icloud.com>

--- models/mart/local_bike/mart_local_bike__ope_performances.sql
+++ models/mart/local_bike/mart_local_bike__ope_performances.sql
@@ -25,6 +25,8 @@ WITH order_perf AS (
 SELECT 
     op.order_id
     , op.order_date
+    , op.shipped_date
+    , op.required_date
     , op.order_status
     , op.customer_id
     , op.customer_name
@@ -37,8 +39,14 @@ SELECT
     , s.staff_id
     , s.staff_name
 
+    , SUM(op.total_items) AS total_items
+    , SUM(op.total_revenue) AS total_revenue
+    , SUM(op.total_net_revenue) AS total_net_revenue
+
 
 FROM order_perf op 
 LEFT JOIN dim_items di ON op.product_id = di.product_id
 LEFT JOIN staff s ON s.staff_id = op.staff_id
 LEFT JOIN stock st ON st.store_id = op.store_id
+
+GROUP BY ALL
