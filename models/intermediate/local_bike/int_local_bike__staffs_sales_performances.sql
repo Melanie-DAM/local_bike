680e474570d8b50b55a99665b9b334bde806d33d
melanie.dam@icloud.com <melanie.dam@icloud.com>
2026-01-04 11:30:58 +0000

    Update staff sales

--- models/intermediate/local_bike/int_local_bike__staffs_sales_performances.sql
+++ models/intermediate/local_bike/int_local_bike__staffs_sales_performances.sql
@@ -15,10 +15,10 @@ WITH staff AS (
         , order_date
         --, EXTRACT(YEAR FROM order_date) AS order_year
         , COUNT(order_id) AS total_orders
-        , ROUND(SUM(total_order_amount),2) AS total_revenue
-        , ROUND(AVG(processing_time_days),2) AS avg_processing_time
+        , SUM(total_order_amount) AS total_revenue
+        , SUM(total_order_amount_net) AS total_revenue_net
+        , AVG(processing_time_days) AS avg_processing_time
     FROM {{ ref('int_local_bike__orders_performances') }}
-    WHERE order_status = 4 -- ventes complétées
     GROUP BY 1, 2, 3
 )
 
@@ -28,12 +28,14 @@ SELECT
     , s.manager_id
     , s.manager_name
     , s.store_id
+    , so.order_date
 
     , so.total_orders
     , so.total_revenue
+    , so.total_revenue_net
     , so.avg_processing_time
 
     -- Revenu moyen par commande pour mesurer la qualité de la vente (upselling)
-    , ROUND(SAFE_DIVIDE(so.total_revenue, so.total_orders),2) AS avg_order_value
+    , SAFE_DIVIDE(so.total_revenue, so.total_orders) AS avg_order_value
 FROM staff s
 LEFT JOIN staff_orders so ON s.staff_id = so.staff_id
