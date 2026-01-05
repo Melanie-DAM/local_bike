4aacfd09e9abb01e2be4f80cf98d0968819487de
Melanie-DAM <melanie.dam@hotmail.fr>
2026-01-05 21:46:06 +0100

    add product name (#19)
    Co-authored-by: Melanie DAM <melanie.dam@icloud.com>

--- models/mart/local_bike/mart_local_bike__fact_orders_analysis.sql
+++ models/mart/local_bike/mart_local_bike__fact_orders_analysis.sql
@@ -2,52 +2,53 @@
 
 WITH orders_perf AS (
     SELECT 
-    order_id
-    , customer_id
-    , order_status
-    -- Status labels
-    , CASE order_status
-        WHEN 1 THEN 'Pending'
-        WHEN 2 THEN 'Processing'
-        WHEN 3 THEN 'Rejected'
-        WHEN 4 THEN 'Completed'
-        ELSE 'Other'
-      END AS order_status_description
-    , order_date
-    , required_date
-    , shipped_date
-    , processing_time_days
-    , days_diff_from_required
-    , is_late_shipping
+        order_id
+        , customer_id
+        , order_status
+        -- Status labels
+        , CASE order_status
+            WHEN 1 THEN 'Pending'
+            WHEN 2 THEN 'Processing'
+            WHEN 3 THEN 'Rejected'
+            WHEN 4 THEN 'Completed'
+            ELSE 'Other'
+        END AS order_status_description
+        , order_date
+        , required_date
+        , shipped_date
+        , processing_time_days
+        , days_diff_from_required
+        , is_late_shipping
 
-    , store_id
-    , staff_id
-    , category_name
-    , brand_name
+        , store_id
+        , staff_id
+        , category_name
+        , brand_name
+        , product_name
 
-    , total_order_amount
-    , total_order_amount_net
-    , total_items_count
+        , total_order_amount
+        , total_order_amount_net
+        , total_items_count
     FROM {{ ref('int_local_bike__orders_performances') }}
 )
 
 , stores AS (
     SELECT
-    store_id
-    , store_name
-    , store_city
-    , store_state
-    , store_zip_code
+        store_id
+        , store_name
+        , store_city
+        , store_state
+        , store_zip_code
     FROM {{ ref('stg_local_bike__stores') }}
 )
 
 , staffs AS (
     SELECT
-    staff_id
-    , staff_name
-    , manager_id
-    , manager_name
-    , store_id
+        staff_id
+        , staff_name
+        , manager_id
+        , manager_name
+        , store_id
     FROM {{ ref('stg_local_bike__staffs') }} 
 )
 
@@ -78,6 +79,7 @@ WITH orders_perf AS (
 
         , ord.category_name
         , ord.brand_name
+        , ord.product_name
 
         , ord.total_order_amount
         , ord.total_order_amount_net
